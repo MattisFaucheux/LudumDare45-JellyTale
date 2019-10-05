68884a9ed150a066eda36ad66b143b7b1764cd84
RENCUREL <pierre.rencurel@orange.fr>
2019-10-05 11:54:25 -0400

    Commented
    DONE : Added comments
    TODO : Merge

--- LudumDare45Project/Assets/_Scripts/Gamemanager.cs
+++ LudumDare45Project/Assets/_Scripts/Gamemanager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Gamemanager : MonoBehaviour
 {
+    //Define all different game states
     private enum GameState
     {
         None = 0,
@@ -20,18 +21,24 @@ public class Gamemanager : MonoBehaviour
     public string messageTriggered;
     public string receivedDeathMessage;
 
-
+    //Store current game state
     private GameState curState;
 
+    //Check player current state
     private bool playerDead = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Setup listeners to enable message reciving
         Messenger.AddListener<GameObject>(messageTriggered, onDeathMessage);
         Messenger.AddListener<GameObject>(receivedDeathMessage, onDeathMessage);
     }
 
+    /// <summary>
+    /// React to player death
+    /// </summary>
+    /// <param name="propType"></param>
     void onDeathMessage(GameObject propType)
     {
         Debug.Log("PlayerDead");
--- LudumDare45Project/Assets/_Scripts/MessengerSystem/PlayerDeath_Message.cs
+++ LudumDare45Project/Assets/_Scripts/MessengerSystem/PlayerDeath_Message.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerDeath_Message : MonoBehaviour
 {
+    //Specify message type and if it was triggered
     public string messageTriggered;
     private bool triggeredOnce = false;
 
 
     public void OnCollisionEnter(Collision _collider)
     {
+        //Check if not triggered yet and if player collides with enemy
         if (!triggeredOnce && _collider.gameObject.tag == "Enemy")
         {
             triggeredOnce = true;
+            //Send message
             Messenger.Broadcast<GameObject>(messageTriggered, this.gameObject);
         }
     }
