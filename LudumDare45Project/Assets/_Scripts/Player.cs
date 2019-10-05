99625f12dee8569411fc18d62583689ba6f5eb0a
Mattis Faucheux <mattis.faucheux@gmail.com>
2019-10-05 09:58:28 -0400

    Creation Dossiers
    Done: created folders  TO DO: player

---
+++ LudumDare45Project/Assets/_Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 position;
+    public KeyCode up;
+    public KeyCode left;
+    public KeyCode right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //test
+        if (Input.GetKey(right))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+      
+        if (Input.GetKey(left))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+
+    }
+}
