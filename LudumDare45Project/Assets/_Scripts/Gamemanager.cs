f15f698575a4a1060d5ba0cb8e9b8641bb37e6d3
RENCUREL <pierre.rencurel@orange.fr>
2019-10-05 11:28:52 -0400

    MessaegFramework
    DONE : Added Message system framework
    TODO : Debug

---
+++ LudumDare45Project/Assets/_Scripts/Gamemanager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    private enum GameState
+    {
+        None = 0,
+        MainMenu,
+        Zone1,
+        Zone2,
+        Zone3,
+        Zone4,
+        Zone5,
+        Zone6,
+        End,
+    };
+
+    private GameState curState;
+
+    private bool playerDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Messenger.AddListener<GameObject>(messageTriggered, onDeathMessage);
+        Messenger.AddListener<GameObject>(receivedDeathMessage, onDeathMessage);
+    }
+
+    void onDeathMessage(GameObject propType)
+    {
+
+    }
+}
---
+++ LudumDare45Project/Assets/_Scripts/MessengerSystem/PlayerDeath_Message.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerEnter_Message : MonoBehaviour
+{
+    public string messageTriggered;
+    private bool triggeredOnce = false;
+
+
+    public void OnTriggerStay(Collider _collider)
+    {
+        Messenger.Broadcast<GameObject>(messageTriggered, this.gameObject);
+        if (!triggeredOnce && Input.GetKey(KeyCode.E))
+        {
+            triggeredOnce = true;
+            Messenger.Broadcast<GameObject>(messageTriggered, this.gameObject);
+        }
+    }
+}
