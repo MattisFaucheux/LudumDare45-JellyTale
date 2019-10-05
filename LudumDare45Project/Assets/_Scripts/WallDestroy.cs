cc9d48daa60a4f0aba031064d9f19a6d225dd8c4
RENCUREL <pierre.rencurel@orange.fr>
2019-10-05 16:32:47 -0400

    WorkingDestruction
    DONE : Wall destruction by boulder
    TODO : Comment and clean code

---
+++ LudumDare45Project/Assets/_Scripts/WallDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDestroy : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Boulder")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
