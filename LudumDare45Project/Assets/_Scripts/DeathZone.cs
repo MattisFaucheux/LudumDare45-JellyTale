2f74b932afa29858bd9b122c3f4b729842d67111
RENCUREL <pierre.rencurel@orange.fr>
2019-10-06 21:31:52 -0400

    DeathZoneImplementation
    DONE : Death zone script
    TODO : Test more extensively and create prefab

---
+++ LudumDare45Project/Assets/_Scripts/DeathZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DeathZone : MonoBehaviour
+{
+    //Specify message type
+
+    public UnityEvent playerDeath;
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        //Check if not triggered yet and if player collides with enemy
+        if (other.gameObject.tag == "Player")
+        {
+            //Send message
+            playerDeath.Invoke();
+        }
+    }
+}
